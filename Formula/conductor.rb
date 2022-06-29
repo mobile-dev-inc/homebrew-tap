fb42602bbd2480af15596696e53f1d1a1bb80a0b
jreleaserbot <jreleaser@kordamp.org>
2022-06-29 19:08:19 +0200

    conductor cli-0.0.2

--- Formula/conductor.rb
+++ Formula/conductor.rb
@@ -1,8 +1,8 @@
-# Generated with JReleaser 1.0.0 at 2022-06-29T19:06:07.606172+02:00
+# Generated with JReleaser 1.0.0 at 2022-06-29T19:08:04.987564+02:00
 class Conductor < Formula
   desc "Conductor CLI"
   homepage "https://conductor.mobile.dev"
-  url "https://github.com/mobile-dev-inc/conductor/releases/download/0.0.2/conductor-0.0.2.zip"
+  url "https://github.com/mobile-dev-inc/conductor/releases/download/cli-0.0.2/conductor-0.0.2.zip"
   version "0.0.2"
   sha256 "c988beeab20251c5bf2141b73fc94e114c1052063f8c260f4ad232a5c531f2e5"
   license "Apache-2.0"
