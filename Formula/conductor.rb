646153b057ad1caacd1de9ba01346610de0b2073
jreleaserbot <jreleaser@kordamp.org>
2022-07-06 08:48:19 +0000

    conductor cli-1.1.1

--- Formula/conductor.rb
+++ Formula/conductor.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.0.0 at 2022-07-05T23:43:03.196766Z
+# Generated with JReleaser 1.0.0 at 2022-07-06T08:46:55.903742Z
 class Conductor < Formula
   desc "Conductor CLI"
   homepage "https://conductor.mobile.dev"
-  url "https://github.com/mobile-dev-inc/conductor/releases/download/cli-1.1.0/conductor-1.1.0.zip"
-  version "1.1.0"
-  sha256 "7bd6d234b306c214b913c8efbb9d7d4407979ff92ea7880e9a56df9014309904"
+  url "https://github.com/mobile-dev-inc/conductor/releases/download/cli-1.1.1/conductor-1.1.1.zip"
+  version "1.1.1"
+  sha256 "6adcc688861948a4239424e40b0604fe8eac325d486510da9205c904b3520e89"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
@@ -16,6 +16,6 @@ class Conductor < Formula
 
   test do
     output = shell_output("#{bin}/conductor --version")
-    assert_match "1.1.0", output
+    assert_match "1.1.1", output
   end
 end
