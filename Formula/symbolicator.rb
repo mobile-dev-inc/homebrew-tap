4f0c29f188829e850709e757ef1855cea7389492
berik <berik@mobile.dev>
2022-08-04 17:36:24 +0400

    Update Symbolicator to v0.0.4

--- Formula/symbolicator.rb
+++ Formula/symbolicator.rb
@@ -1,10 +1,10 @@
 class Symbolicator < Formula
   desc "Symbolize crash and memory leak reports"
   homepage "https://github.com/mobile-dev-inc/Symbolicator"
-  url "https://github.com/mobile-dev-inc/Symbolicator/archive/refs/tags/v0.0.3.tar.gz"
+  url "https://github.com/mobile-dev-inc/Symbolicator/archive/refs/tags/v0.0.4.tar.gz"
   head "https://github.com/mobile-dev-inc/Symbolicator.git", branch: "main"
-  version "0.0.3"
-  sha256 "45c7629a8cdfeb4372d3b48c0d9a326d68891b1fb534f4ebb565c20623b612dc"
+  version "0.0.4"
+  sha256 "21dacd389d77b8c1771dadceedfa73842315372bab7508c5a0f6367ba40c4a40"
   license "http://www.apache.org/licenses/LICENSE-2.0"
 
   depends_on xcode: ["13.4", :build]
