8ab370773f5b2a2dbf1eb57a0533e0dc317c31fb
jreleaserbot <jreleaser@kordamp.org>
2022-11-14 14:32:22 +0000

    maestro cli-1.14.0

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -1,13 +1,13 @@
-# Generated with JReleaser 1.0.0 at 2022-11-10T13:35:32.856035Z
+# Generated with JReleaser 1.0.0 at 2022-11-14T14:31:01.465Z
 class Maestro < Formula
   desc "Maestro CLI"
   homepage "https://maestro.mobile.dev"
-  url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.13.2/maestro.zip"
-  version "1.13.2"
-  sha256 "a6a1e1d267196fa1268b79146426d43f834a7873b15ebf36b8857fa1d8b19bdc"
+  url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.14.0/maestro.zip"
+  version "1.14.0"
+  sha256 "e199df62bdfc94a03ab81ddbf9680d5e2aabe4ef45bedc92c37f95adfb83ef0a"
   license "Apache-2.0"
 
-  depends_on "openjdk@11"
+  depends_on "openjdk@8"
 
   def install
     libexec.install Dir["*"]
@@ -16,6 +16,6 @@ class Maestro < Formula
 
   test do
     output = shell_output("#{bin}/maestro --version")
-    assert_match "1.13.2", output
+    assert_match "1.14.0", output
   end
 end
