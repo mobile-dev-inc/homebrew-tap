b96da68f82e4a4fc3b1bdb1a9e6dbae63d74d3c1
jreleaserbot <jreleaser@kordamp.org>
2023-11-10 12:47:23 +0000

    maestro cli-1.34.1

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.0.0 at 2023-10-31T20:35:45.792Z
+# Generated with JReleaser 1.0.0 at 2023-11-10T12:43:23.287Z
 class Maestro < Formula
   desc "Maestro CLI"
   homepage "https://maestro.mobile.dev"
-  url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.34.0/maestro.zip"
-  version "1.34.0"
-  sha256 "d9a04429c067b8c2c9bfb7e9d40f3c20676cc78407886c66a9397846d1507be1"
+  url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.34.1/maestro.zip"
+  version "1.34.1"
+  sha256 "2da465ff8abec348ecc47e92cd22517db4341dc79814319209400e751c831672"
   license "Apache-2.0"
 
 
@@ -15,6 +15,6 @@ class Maestro < Formula
 
   test do
     output = shell_output("#{bin}/maestro --version")
-    assert_match "1.34.0", output
+    assert_match "1.34.1", output
   end
 end
