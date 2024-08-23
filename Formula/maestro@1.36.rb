cfcceb1741d7e66f1ff74059d78df2ac8372a966
Bartek Pacia <barpac02@gmail.com>
2024-08-23 10:12:59 +0100

    Delete old, obsolete formulas (#5)

--- Formula/maestro@1.36.rb
+++ Formula/maestro@1.36.rb
@@ -1,7 +1,7 @@
 # Generated with JReleaser 1.0.0 at 2024-02-15T09:26:56.36Z
 # Then manually updated by Bartek Pacia
 class MaestroAT136 < Formula
-  desc "Maestro CLI"
+  desc "The simplest and most effective mobile UI testing framework."
   homepage "https://maestro.mobile.dev"
   url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.36.0/maestro.zip"
   version "1.36.0"
