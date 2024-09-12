14a5d2ab054e7829e8028e827d35a2cc3156a4a0
Bartek Pacia <barpac02@gmail.com>
2024-09-12 21:57:02 +0200

    fix class name of formula `maestro@1.39-dev.2`

--- Formula/maestro@1.39-dev.2.rb
+++ Formula/maestro@1.39-dev.2.rb
@@ -1,6 +1,6 @@
 # Generated with JReleaser 1.13.1 at 2024-08-15T02:26:11.611Z
 # Then manually updated by Bartek Pacia
-class MaestroAT139Dev1 < Formula
+class MaestroAT139Dev2 < Formula
   desc "The simplest and most effective mobile UI testing framework."
   homepage "https://maestro.mobile.dev"
   url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.39.0-dev.2/maestro.zip"
