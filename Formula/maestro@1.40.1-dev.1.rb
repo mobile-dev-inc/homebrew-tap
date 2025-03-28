61404d0dd6f9b0da5f414ac85f066ed9cd97f4a1
Proksh Luthra <35415752+proksh@users.noreply.github.com>
2025-03-28 16:27:23 +0530

    added pre lease 1.40.1

---
+++ Formula/maestro@1.40.1-dev.1.rb
@@ -0,0 +1,21 @@
+# Generated with JReleaser 1.13.1 at 2024-08-15T02:26:11.611Z
+# Then manually updated by Bartek Pacia
+class MaestroAT140Dev1 < Formula
+  desc "The simplest and most effective mobile UI testing framework."
+  homepage "https://maestro.mobile.dev"
+  url "https://github.com/mobile-dev-inc/Maestro/releases/download/cli-1.40.1-dev.1/maestro.zip"
+  version "1.40.1"
+  license "Apache-2.0"
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/maestro" => "maestro"
+  end
+
+  test do
+    output = shell_output("#{bin}/maestro --version")
+    assert_match "1.40.1", output
+  end
+end
