3e0e45afa9dd2c02a5e000fec6e5d1d1f8694dc0
jreleaserbot <jreleaser@kordamp.org>
2025-05-07 07:16:20 +0000

    maestro cli-1.40.2

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.13.1 at 2025-05-05T08:15:29.241597Z
+# Generated with JReleaser 1.13.1 at 2025-05-07T07:13:39.191832Z
 
 class Maestro < Formula
   desc "The easiest way to automate UI testing for your mobile app"
   homepage "https://maestro.mobile.dev"
-  url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.40.1/maestro.zip"
-  version "1.40.1"
-  sha256 "f1339fc7f26b8de924bb3d3dadd4408eec015e7e4e38c2332b839fc7cf2774f9"
+  url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-1.40.2/maestro.zip"
+  version "1.40.2"
+  sha256 "fc961648dfd8e46379a6d691a7d188b615033458d4fbacce242d875e396f9721"
   license "Apache-2.0"
 
   depends_on "openjdk"
@@ -17,6 +17,6 @@ class Maestro < Formula
 
   test do
     output = shell_output("#{bin}/maestro --version")
-    assert_match "1.40.1", output
+    assert_match "1.40.2", output
   end
 end
