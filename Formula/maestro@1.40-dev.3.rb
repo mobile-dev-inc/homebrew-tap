c084c4d22d26de54de5e7d7da58984f8fe151ad3
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2025-05-08 23:34:44 +0530

    fix: forgot changing the tap class name

--- Formula/maestro@1.40-dev.3.rb
+++ Formula/maestro@1.40-dev.3.rb
@@ -1,6 +1,6 @@
 # Generated with JReleaser 1.13.1 at 2024-08-15T02:26:11.611Z
 # Then manually updated by Bartek Pacia
-class MaestroAT140Dev2 < Formula
+class MaestroAT140Dev3 < Formula
   desc "The simplest and most effective mobile UI testing framework."
   homepage "https://maestro.mobile.dev"
   url "https://github.com/mobile-dev-inc/Maestro/releases/download/cli-1.40.3-dev.1/maestro.zip"
