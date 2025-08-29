9c159685900fb74d683f94452f19f57b18afabce
jreleaserbot <jreleaser@kordamp.org>
2025-08-29 16:17:34 +0000

    maestro cli-2.0.1

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -1,14 +1,14 @@
-# Generated with JReleaser 1.13.1 at 2025-08-29T15:21:04.565902885Z
+# Generated with JReleaser 1.13.1 at 2025-08-29T16:13:20.321086209Z
 
 class Maestro < Formula
   desc "The easiest way to automate UI testing for your mobile app"
   homepage "https://maestro.mobile.dev"
   url "https://github.com/mobile-dev-inc/maestro/releases/download/cli-2.0.1/maestro.zip"
   version "2.0.1"
-  sha256 "d75a0e6c15b63104e649fe1708acad8e6ede7212e36aebe8d37dc888b45867e8"
+  sha256 "7d94484fd6d090b113648ffc687e099a3c54c02b23c8d86224cf36bd32270b1a"
   license "Apache-2.0"
 
-  depends_on "openjdk"
+  depends_on "openjdk" => "17+"
 
   def install
     libexec.install Dir["*"]
