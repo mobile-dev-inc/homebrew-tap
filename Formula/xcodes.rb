d9e2562f5a53faf59581ddf0ba162973db753bbf
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2026-02-19 11:38:29 +0530

    feat: add xcodes archive formula

---
+++ Formula/xcodes.rb
@@ -0,0 +1,21 @@
+class Xcodes < Formula
+  desc "The best command-line tool to install and switch between multiple versions of Xcode"
+  homepage "https://github.com/mobile-dev-inc/xcodes"
+  url "https://github.com/mobile-dev-inc/xcodes/archive/93bce877531e4bd60f7ef7f14379360a85caae36.tar.gz"
+  version "2025.09.26"
+  sha256 "aa4f02703703df06446d8efcda4e7dcd89cc3c05bf577ab1734b24e682f00c5c"
+  license "MIT"
+
+  depends_on xcode: ["13.4", :build]
+
+  def install
+    system "swift", "build", "--configuration", "release", "--disable-sandbox"
+
+    system "install", "-d", "#{prefix}/bin"
+    system "cp", ".build/release/xcodes", "#{prefix}/bin/xcodes"
+  end
+
+  test do
+    system "#{bin}/xcodes", "--version"
+  end
+end
