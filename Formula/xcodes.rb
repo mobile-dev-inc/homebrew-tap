ef67713a2fa1004399e5926049ba17fc0e3c36fd
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2026-02-27 17:51:41 +0530

    bump: xcodes release

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,9 +1,9 @@
 class Xcodes < Formula
   desc "The best command-line tool to install and switch between multiple versions of Xcode"
   homepage "https://github.com/mobile-dev-inc/xcodes"
-  url "https://github.com/mobile-dev-inc/xcodes/archive/11112a7829c379acbc37bad371fd5d526e9fbceb.tar.gz"
+  url "https://github.com/mobile-dev-inc/xcodes/archive/5ce5aa622aa2b7ebcdc367320eeab80543fd2a55.tar.gz"
   version "2026.02.27"
-  sha256 "7a83317a97373fb92f4fcc363cb48c0931288f1df775c905a44ff543d09ad434"
+  sha256 "12507f4831815624f79eaf6a6a709467f9029a3276e6da896c0b33597e9d6545"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
